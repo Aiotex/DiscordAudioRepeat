9c12bb9601d288027f30ef84829f885ba018eb17
Jiron <59139789+Jiron@users.noreply.github.com>
2023-03-05 05:14:14 +0100

    feat: some bug fixes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { Client, GatewayIntentBits } = require('discord.js');
 const { joinVoiceChannel, EndBehaviorType, AudioPlayerStatus, StreamType, createAudioPlayer, createAudioResource, getVoiceConnection, NoSubscriberBehavior } = require("@discordjs/voice");
 const { OpusEncoder } = require( "@discordjs/opus" ); 
 const dotenv = require("dotenv");
+const fs = require('fs');
 dotenv.config();
 
 const client = new Client({
@@ -19,30 +20,84 @@ client.on("messageCreate", (message) => {
     joinVoiceChannel({
         channelId: voicechannel.id,
         guildId: message.guild.id,
-        adapterCreator: message.guild.voiceAdapterCreator
+        adapterCreator: message.guild.voiceAdapterCreator,
+        selfDeaf: false
     });
     let connection = getVoiceConnection(message.guild.id);
+    let connectedUsers = [];
 
-    listenAndRepeat();
-    function listenAndRepeat() {
-        const opusStream = connection.receiver.subscribe(message.member.id, {
-            end: {
-                behavior: EndBehaviorType.Manual
-            },
-        });
-        const audioPlayer = createAudioPlayer();
-        connection.subscribe(audioPlayer);
-        const resource = createAudioResource(opusStream, { inputType: StreamType.Opus });
-        audioPlayer.play(resource);
-    
-        audioPlayer.on(AudioPlayerStatus.Idle, () => {
-            audioPlayer.stop();
-            opusStream.destroy();
-            setTimeout(() => {
-                listenAndRepeat();
-            }, 10);
+    voicechannel.members.forEach((member) => {
+        if(member.user.bot) return;
+        connectedUsers.push(member.id);
+    });
+
+    client.on('voiceStateUpdate', (oldMember, newMember) => {
+        console.log(oldMember, newMember)
+        let newUserChannel = newMember.channel?.id;
+        let oldUserChannel = oldMember.channel?.id;
+
+        if(newMember.member.user.bot) return;
+
+        if(newUserChannel === voicechannel.id) {
+            if(connectedUsers.includes(newMember.id)) return;
+            connectedUsers.push(newMember.id);
+            listenAndRepeat(newMember.id);
+        } else {
+            connectedUsers = connectedUsers.filter(x => x != newMember.id);
+        }
+    });
+
+    let talking;
+
+    setInterval(() => {
+        if(talking == null) {
+            let speaking = Array.from(connection.receiver.speaking.users)?.toString().split(',')[0];
+            talking = speaking == "" ? null : speaking;
+            if(talking != null) initListeners();
+        }
+    }, 50);
+
+    function initListeners() {
+        connectedUsers.forEach(connectedUser => {
+            listenAndRepeat(connectedUser);
         });
     }
+
+    initListeners();
+
+    function listenAndRepeat(userId) {
+        if(talking != userId) {
+            return;
+        } else {
+            talking = userId;
+            const opusStream = connection.receiver.subscribe(userId, {
+                end: {
+                    behavior: EndBehaviorType.Manual
+                },
+            });
+    
+            const audioPlayer = createAudioPlayer();
+            connection.subscribe(audioPlayer);
+            const resource = createAudioResource(opusStream, { inputType: StreamType.Opus });
+            audioPlayer.play(resource);
+        
+            audioPlayer.on(AudioPlayerStatus.Idle, () => {
+                audioPlayer.stop();
+                opusStream.destroy();
+                setTimeout(() => {
+                    talking = null;
+                    if(!connectedUsers.includes(userId)) return;
+                    listenAndRepeat(userId);
+                }, 10);
+            });
+        }
+    }
+
+    let cnt = 0;
+    setInterval(() => {
+        cnt++;
+        console.log(connectedUsers, cnt);
+    }, 500);
 });
 
 client.login(process.env.TOKEN);
